da7db82e9c34971c0bdb4091ce64e76272d9fdc8
ananth <ananthprasad91@gmail.com>
2020-02-12 23:40:15 +0530

    Tried Jest

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { Component, lazy, Suspense } from 'react';
 import { Route, BrowserRouter as Router, Switch } from "react-router-dom"
+import { Provider } from "react-redux"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import store from "./store"
 import { ProtectedRoute } from "./common/ProtectedRoute"
 
 const LoginPage = lazy(() => import('./pages/LoginPage'));
@@ -15,21 +17,23 @@ export default class App extends Component {
 
   render() {
     return (
-      <Router>
-        <div className="app">
-          <Suspense fallback={<div>Loading...</div>}>
-            <Switch>
-              <Route exact path="/" component={LoginPage} />
-              <Route exact path="/login" component={LoginPage} />
-              <Route exact path="/register" component={RegisterPage} />
-              <Route exact path="/reset" component={PasswordResetPage} />
-              <ProtectedRoute exact path="/dashboard" component={DashboardPage} />
-              <ProtectedRoute exact path="/createStory" component={CreateStoryPage} />
-              <ProtectedRoute exact path="/manageStory" component={ManageStoriesPage} />
-            </Switch>
-          </Suspense>
-        </div>
-      </Router>
+      <Provider store={store}>
+        <Router>
+          <div className="app">
+            <Suspense fallback={<div>Loading...</div>}>
+              <Switch>
+                <Route exact path="/" component={LoginPage} />
+                <Route exact path="/login" component={LoginPage} />
+                <Route exact path="/register" component={RegisterPage} />
+                <Route exact path="/reset" component={PasswordResetPage} />
+                <ProtectedRoute exact path="/dashboard" component={DashboardPage} />
+                <ProtectedRoute exact path="/createStory" component={CreateStoryPage} />
+                <ProtectedRoute exact path="/manageStory" component={ManageStoriesPage} />
+              </Switch>
+            </Suspense>
+          </div>
+        </Router>
+      </Provider>
     )
   }
 }
---
+++ src/test/components/Login.test.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import renderer from 'react-test-renderer';
+import configureStore from 'redux-mock-store';
+import MyConnectedComponent from '.';
+const mockStore = configureStore([]);
+describe('My Connected React-Redux Component', () => {
+  let store;
+  let component;
+  beforeEach(() => {
+    store = mockStore({
+      myState: 'sample text',
+    });
+    component = renderer.create(
+      <Provider store={store}>
+        <MyConnectedComponent />
+      </Provider>
+    );
+  });
+  it('should render with given state from Redux store', () => {
+  });
+  it('should dispatch an action on button click', () => {
+  });
+});
